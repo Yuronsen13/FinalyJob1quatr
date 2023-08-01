53f7dd8bd465c9c892df8ae88df880b6833ad172
Yura <you@example.com>
2023-08-01 14:24:11 +0300

    Добавили gitignore

--- Tesk/Program.cs
+++ Tesk/Program.cs
@@ -5,100 +5,100 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-// string[] array1 = new string[4] {"HELLO", "2", "WORLD", ":)"};
-// string[] array2 = new string[array1.Length];
+string[] array1 = new string[4] {"HELLO", "2", "WORLD", ":)"};
+string[] array2 = new string[array1.Length];
 
-// void NewArray(string[] array1, string[] array2)
-// {
-//     int count = 0;
-//     for (int i = 0; i < array1.Length; i++)
-//     {
-//     if(array1[i].Length <= 3)
-//         {
-//         array2[count] = array1[i];
-//         count++;
-//         }
-//     }
-// }
-
-// void PrintArray(string[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
-// NewArray(array1, array2);
-// PrintArray(array2);
-
-
-int m = 3;
-int size = 4;
-string[]array1 = new string[size];
-Console.WriteLine($"Введите количество {size} заданных слов через пробел");
-
-FillArray(array1);
-PrintArray(array1);
-Console.Clear();
-
-void FillArray(string[]array)
-{
-    for (int i=0; i<size; i++)
-    {
-        array[i]=Console.ReadLine();
-        i++;
-    }
-}
-
-void PrintArray(string[]array)
-{
-    int arrayLenght = array.Lenght
-    for (int i=0; i<array.Lenght; i++)
-    {
-        Console.WriteLine(array[i]+ " ");
-    }
-}
-
-
-
-void GetTwoSizeArray(string[]array)
+void NewArray(string[] array1, string[] array2)
 {
     int count = 0;
-    for (int i = 0; i<size; i++)
+    for (int i = 0; i < array1.Length; i++)
     {
-        if (array[i].Length <= m)
+    if(array1[i].Length <= 3)
         {
-            count++;
+        array2[count] = array1[i];
+        count++;
         }
     }
-    return count;
 }
 
-
-
-    if GetTwoSizeArray((array1)==0)
+void PrintArray(string[] array)
 {
-    Console.WriteLine($"Искомого элемента нет");
-}
-    else
-{
-    string[]array2 = TransferElements[array1];
-    PrintArray(array2);
-}
-
-
-
-string[]TransferElements(string[]array)
-{
-    string[]array2 = new string[GetTwoSizeArray(array1)];
-    for (int i = 0; int j = 0; i < size; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Lenght <= m)
-        {
-            array2[i]=array1[i];
-            j++;
-        }
+        Console.Write($"{array[i]} ");
     }
-    return array2;
+    Console.WriteLine();
 }
+NewArray(array1, array2);
+PrintArray(array2);
+
+
+// int m = 3;
+// int size = 4;
+// string[]array1 = new string[size];
+// Console.WriteLine($"Введите количество {size} заданных слов через пробел");
+
+// FillArray(array1);
+// PrintArray(array1);
+// Console.Clear();
+
+// void FillArray(string[]array)
+// {
+//     for (int i=0; i<size; i++)
+//     {
+//         array[i]=Console.ReadLine();
+//         i++;
+//     }
+// }
+
+// void PrintArray(string[]array)
+// {
+//     int arrayLenght = array.Lenght
+//     for (int i=0; i<array.Lenght; i++)
+//     {
+//         Console.WriteLine(array[i]+ " ");
+//     }
+// }
+
+
+
+// void GetTwoSizeArray(string[]array)
+// {
+//     int count = 0;
+//     for (int i = 0; i<size; i++)
+//     {
+//         if (array[i].Length <= m)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+
+//     if GetTwoSizeArray((array1)==0)
+// {
+//     Console.WriteLine($"Искомого элемента нет");
+// }
+//     else
+// {
+//     string[]array2 = TransferElements[array1];
+//     PrintArray(array2);
+// }
+
+
+
+// string[]TransferElements(string[]array)
+// {
+//     string[]array2 = new string[GetTwoSizeArray(array1)];
+//     for (int i = 0; int j = 0; i < size; i++)
+//     {
+//         if (array[i].Lenght <= m)
+//         {
+//             array2[i]=array1[i];
+//             j++;
+//         }
+//     }
+//     return array2;
+// }
